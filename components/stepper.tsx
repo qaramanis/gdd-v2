76f11bc8fe319d557779607f6711b1595b6c9f70
Apostolos Karamanis <apostkaram@gmail.com>
2025-04-07 07:54:09 +0300

    minor fix

--- components/stepper.tsx
+++ components/stepper.tsx
@@ -344,9 +344,7 @@ function StepConnector({ isComplete }: StepConnectorProps) {
   );
 }
 
-interface CheckIconProps extends React.SVGProps<SVGSVGElement> {}
-
-function CheckIcon(props: CheckIconProps) {
+function CheckIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
